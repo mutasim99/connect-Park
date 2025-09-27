803f110f1b693d05c7015c80df4648d3e83bd64a
mutasim99 <virginmr882@gmail.com>
2025-09-27 12:33:18 +0600

    added features section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 
 import Banner from "./components/Home/Banner";
+import Features from "./components/Home/Features";
 
 export default function Home() {
   return (
     <div>
       <Banner></Banner>
+      <Features></Features>
     </div>
   );
 }
